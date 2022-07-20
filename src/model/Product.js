03f7117959d54f079249b326b4b12251af4dd322
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-20 20:34:35 -0300

    chore: Product model

--- src/model/Product.js
+++ src/model/Product.js
@@ -1,1 +1,18 @@
 'use strict';
+
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    barCode: { type: Number, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    amount: { type: Number, required: true },
+  },
+  { versionKey: false },
+);
+
+const Product = mongoose.model('Product', ProductSchema, 'products');
+
+export default Product;
