701d10e82788049a26284751b13d3944af66e9e8
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-20 20:37:29 -0300

    chore: initial file config

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,1 +1,3 @@
 'use strict';
+
+import {} from '../services/product.services.js';
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,1 +1,9 @@
 'use strict';
+
+import express from 'express';
+const router = express.Router();
+
+import {} from '../controllers/product.controllers.js';
+import {} from '../middleware/product.middleware.js';
+
+export default router;
