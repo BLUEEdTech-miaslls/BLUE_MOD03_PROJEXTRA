71e85d87f3c1924a431f2d1d7f7a8aae21e30e86
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-20 20:37:37 -0300

    chore: index config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import databaseConnection from './database/dbConnection.js';
+import productRoutes from './routes/product.routes.js';
+import {} from './middleware/product.middleware.js';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+databaseConnection();
+
+app.use(cors());
+app.use(express.json());
+app.use('/product', productRoutes);
+
+app.listen(port, () => console.log(`server running @ port ${port}`));
